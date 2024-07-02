6816aee30e58b5045833074b1bc4a93f6b1e9810
Akarsh8933 <akarshkatiyarak123@gmail.com>
2024-07-02 18:09:29 +0530

    Added validator to auth.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -20,6 +20,7 @@ const UserSchema = new Schema({
         default: Date.now
     }
 });
+const User = mongoose.model('user',UserSchema);
+User.createIndexes();
 
-
-module.exports = mongoose.model('user',UserSchema);
+module.exports = User;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,31 @@
 const express = require('express')
 const User = require('../models/User')
-
+const { body, validationResult } = require('express-validator');
 const router = express.Router();
 
 
 //create a user using POST: "/api/auth/" -- it doesn't require user to be authorized
-router.post('/',(req,res)=>{
-    const user = User(req.body);
-    user.save();
-    res.send(req.body);
-})
+router.post('/',[
+    body('name','Enter a valid name').isLength({min:3}),
+    body('email','Enter a valid email').isEmail(),
+    body('password', 'Password must have atleast 5 characters').isLength({min:5})
+],(req,res)=>{
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      // handle validation errors
+      return res.status(400).json({result : result.array()});
+    }
+    // const user = User(req.body);
+    // user.save();
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+
+    }).then(user=>res.json(user))
+    .catch(err=> {console.log(err)
+        res.json({error:err.message})})
+    })
+    
 
 module.exports = router;
