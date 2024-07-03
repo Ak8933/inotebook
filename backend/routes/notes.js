cfe1ea78130db412d5c82c8006baebddfd1810f8
Akarsh8933 <akarshkatiyarak123@gmail.com>
2024-07-03 22:38:07 +0530

    created endpoint to fetch all notes 'routes/notes.js' and added a foreign key in models/Notes.js schema .

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,10 +1,12 @@
 const express = require('express')
-
+const Notes = require('../models/Notes');
 const router = express.Router();
+const fetchuser = require('../middleware/fetchuser');
 
-router.get('/',(req,res)=>{
-    console.log(req.body)
-    res.json([]);
+// ROUTE : 1 -- Get all notes using GET "/api/"
+router.get('/fetchallnotes',fetchuser, async(req,res)=>{
+    const notes = await Notes.find({user:req.user.id});
+    res.json(notes);
 })
 
 module.exports = router;
