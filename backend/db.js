18da23c88098638516a6ced545ab4e6aad17f593
Akarsh8933 <akarshkatiyarak123@gmail.com>
2024-08-17 14:41:05 +0530

    bugs

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/inotebook";
+// const mongoURI = "mongodb://localhost:27017/inotebook";
 const dotenv = require('dotenv')
 dotenv.config();
 
 
 const connectToMongo = async () => {
     try {
-        await mongoose.connect(mongoURI || process.env.MONGO_URI);
+        await mongoose.connect( process.env.MONGO_URI);
         console.log("Connected to Mongo Successfully");
     } catch (error) {
         console.error("Error connecting to Mongo:", error);
