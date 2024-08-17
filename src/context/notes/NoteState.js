d8d54c45b14b46ae7b252855064b5d609262624d
Akarsh8933 <akarshkatiyarak123@gmail.com>
2024-08-17 15:19:09 +0530

    bug

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
-    const host = "http://localhost:5000" || "https://inotebook-backend-o4th.onrender.com"
+    const host = "https://inotebook-backend-o4th.onrender.com"
     const notesInitial = []
     const [notes, setNotes] = useState(notesInitial);
 
